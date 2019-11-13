0c560dc1d5c501140e06602c9a41bd7afc8d0409
woetroe0803 <wbunthof@gmail.com>
2019-11-13 15:15:33 +0100

    added function to get question per questionnaire part

--- app/Vraag.php
+++ app/Vraag.php
@@ -6,9 +6,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class vraag extends Model
 {
+    static function perOnderdeel(Formonderdeel $onderdeel) {
+        return Vraag::where('formonderdeel_id', $onderdeel->id)->get();
+    }
+
+    //Relations
     public function antwoord()
     {
       return $this->hasMany('App\Antwoord');
