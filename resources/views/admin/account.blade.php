79c5abbbdd59d295e3a56c67bfeeb6fac11a69f2
woetroe0803 <wbunthof@gmail.com>
2019-12-10 11:45:27 +0100

    updated account.blade.php because it wasnt working

--- resources/views/admin/account.blade.php
+++ resources/views/admin/account.blade.php
@@ -13,23 +13,17 @@
   @endphp
 
 
-  @for ($i=0; $i < count($index); $i++)
-    @php
-      $indexStr = (string)$index[$i];
-    @endphp
-
-    {!! Form::open(['url' => route('admin.account'), 'method' => 'POST']) !!}
-    {!! Form::token() !!}
-    {!! Form::hidden('_method', 'PUT') !!}
-    {!! Form::hidden('onderdeel', $indexStr) !!}
-    {!! Form::hidden('id',  Auth::User()->id) !!}
-    <div class="form-group mb-2">
-      <label for="static{{$i}}" class="sr-only">{{ucfirst($indexStr)}}</label>
-      <input type="text" readonly class="form-control-plaintext" id="static{{$i}}" value="{{ucfirst($indexStr)}}">
+    @foreach($index as $key => $onderdeel)
+        {!! Form::open(['url' => route('admin.account'), 'method' => 'POST', 'class' => 'form-inline']) !!}
+        {!! Form::token() !!}
+        {!! Form::hidden('_method', 'PUT') !!}
+        <div class="form-group mb-2">
+            <label for="static{{$key}}" class="sr-only">{{ucfirst($onderdeel)}}</label>
+            <input type="text" readonly class="form-control-plaintext" id="static{{$key}}" value="{{ucfirst($onderdeel)}}">
     </div>
-    {!! Form::text($indexStr, Auth::User()->$indexStr, ['name' => 'waarde', 'class' => 'form-group']) !!}
-    {!! Form::submit('Opslaan', ['class' => 'btn btn-primary mb-2']) !!}
-    {!! Form::close() !!}
-    @endfor
+        {!! Form::text($onderdeel, Auth::User()->$onderdeel, ['name' => $onderdeel, 'class' => 'form-control', 'style' => 'margin-right: 8px;']) !!}
+        {!! Form::submit('Opslaan', ['class' => 'btn btn-primary mb-2']) !!}
+        {!! Form::close() !!}
+    @endforeach
 </div>
 @endsection
