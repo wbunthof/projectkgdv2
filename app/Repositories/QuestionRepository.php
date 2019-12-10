fdc2c58936b75bbf5ea2091468ad2498506e83b3
woetroe0803 <wbunthof@gmail.com>
2019-12-10 12:07:01 +0100

    Created files for question service/repository

---
+++ app/Repositories/QuestionRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Repositories;
+
+use App\Vraag;
+
+class QuestionRepository
+{
+    protected $question;
+
+    public function __construct(Vraag $question)
+    {
+        $this->question = $question;
+    }
+
+    public function create($attributes)
+    {
+        return $this->question->create($attributes);
+    }
+
+    public function all()
+    {
+        return $this->question->all();
+    }
+
+    public function find($id)
+    {
+        return $this->question->find($id);
+    }
+
+    public function update($id, array $attributes)
+    {
+        return $this->question->find($id)->update($attributes);
+    }
+
+    public function delete($id)
+    {
+        return $this->question->find($id)->delete();
+    }
+}
---
+++ app/Services/QuestionService.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace App\Services;
+
+use App\Repositories\QuestionRepository;
+use Illuminate\Http\Request;
+
+class QuestionService
+{
+    protected $question;
+
+    public function __construct(QuestionRepository $question)
+    {
+        $this->question = $question;
+    }
+
+    public function index()
+    {
+        return $this->question->all();
+    }
+
+    public function create(Request $request)
+    {
+
+        $attributes = $request->all();
+
+        return $this->question->create($attributes);
+    }
+
+    public function read($id)
+    {
+        return $this->question->find($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $attributes = $request->all();
+        return $this->question->update($id, $attributes);
+    }
+
+    public function delete($id)
+    {
+        return $this->question->delete($id);
+    }
+}
