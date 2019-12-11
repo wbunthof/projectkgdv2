ece6f49f16bff84c8a43096c0b3e945fde18dc75
woetroe0803 <wbunthof@gmail.com>
2019-12-11 12:40:44 +0100

    Added a new type of messages, which is used in gilde.messages

--- resources/views/admin/includes/messages.blade.php
+++ resources/views/admin/includes/messages.blade.php
@@ -29,3 +29,21 @@
     {{session('error')}}
   </div>
 @endif
+
+@if (session('succes'))
+    <div class="alert alert-success alert-dismissible">
+        {{session('succes')}}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
+
+@if (session('error'))
+    <div class="alert alert-danger alert-dismissible">
+        {{session('error')}}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
