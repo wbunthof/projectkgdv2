d1c4e42713aed4eeb866a66995d2f1678226e4c3
woetroe0803 <wbunthof@gmail.com>
2019-12-11 12:39:16 +0100

    Created service/repository for Answer

---
+++ app/Repositories/AnswerRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Repositories;
+
+use App\Vraag;
+
+class AnswerRepository
+{
+    protected $answer;
+
+    public function __construct(Antwoord $answer)
+    {
+        $this->answer = $answer;
+    }
+
+    public function create($attributes)
+    {
+        return $this->answer->create($attributes);
+    }
+
+    public function all()
+    {
+        return $this->answer->all();
+    }
+
+    public function find($id)
+    {
+        return $this->answer->find($id);
+    }
+
+    public function update($id, array $attributes)
+    {
+        return $this->answer->find($id)->update($attributes);
+    }
+
+    public function delete($id)
+    {
+        return $this->answer->find($id)->delete();
+    }
+}
---
+++ app/Services/AnswerService.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\Services;
+
+use App\Repositories\AnswerRepository;
+use Illuminate\Http\Request;
+
+class AnswerService
+{
+    protected $answer;
+
+    public function __construct(AnswerRepository $answer)
+    {
+        $this->answer = $answer;
+    }
+
+    public function index()
+    {
+        return $this->answer->all();
+    }
+
+    public function create(Request $request)
+    {
+
+        $attributes = $request->all();
+        return $this->answer->create($attributes);
+    }
+
+    public function read($id)
+    {
+        return $this->answer->find($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $attributes = $request->all();
+        return $this->answer->update($id, $attributes);
+    }
+
+    public function delete($id)
+    {
+        return $this->answer->delete($id);
+    }
+}
