40154eab99475f84214cbfa19237998cd9f51e29
woetroe0803 <wbunthof@gmail.com>
2020-01-15 17:21:20 +0100

    Redirect after raadsheer logout to raadsheer login page instead of gilde login page

--- resources/views/raadsheer/includes/navbar.blade.php
+++ resources/views/raadsheer/includes/navbar.blade.php
@@ -43,7 +43,7 @@
                       {{ __('Logout') }}
                   </a>
 
-                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  <form id="logout-form" action="{{ route('raadsheer.logout') }}" method="POST" style="display: none;">
                       @csrf
                   </form>
               </div>
