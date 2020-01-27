d15920f74733de157f4b43b0d12f5a938b7b8202
woetroe0803 <wbunthof@gmail.com>
2020-01-27 18:01:10 +0100

    Delete users mail added

---
+++ resources/views/mails/deleteUser.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Hallo {{ $user->name }}
+
+U bent verwijderd van het online inschrijfformulier.  
+U kunt dus niet meer inloggen.
+
+Voor vragen neem contact op met het organiserend gilde op <a href="mailto:{{ setting('email organiserend gilde') }}">{{ setting('email organiserend gilde') }}</a>.
+
+Bedankt voor het gebruik van de onlinedienst,<br>
+{{ config('app.name') }}
+@endcomponent
+H
